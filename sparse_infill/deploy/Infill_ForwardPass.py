44a8a9c6ff1fbca2bd3daa8cb06f263e4c3ed2ad
twongjirad <taritree.wongjirad@gmail.com>
2020-07-01 12:28:46 -0400

    removed excess imports. handle pytorch 1.3 weight format. allow way to specify weight location

--- sparse_infill/deploy/Infill_ForwardPass.py
+++ sparse_infill/deploy/Infill_ForwardPass.py
@@ -1,7 +1,7 @@
 #!/bin/env python
+from __future__ import print_function
 
 ## IMPORT
-
 # python,numpy
 import os,sys,commands
 import shutil
@@ -9,37 +9,30 @@ import time
 import traceback
 import numpy as np
 
-# import larcv
-from larcv import larcv
-
 # torch
 import torch
-import torch.nn as nn
-import torch.nn.parallel
-import torch.backends.cudnn as cudnn
-import torch.distributed as dist
-import torch.optim
-import torch.utils.data
-import torch.utils.data.distributed
-import torchvision.transforms as transforms
-import torchvision.datasets as datasets
-import torchvision.models as models
-import torch.nn.functional as F
 
-sys.path.append("../models")
-from sparseinfill import SparseInfill
+# import larcv
+import ROOT
+from larcv import larcv
 
-def forwardpassu(data_t):
+#sys.path.append("../models")
+# must be in python path
+from sparseinfill import SparseInfill    
+
+def forwardpassu(data_t, CHECKPOINT_FILE=None):
+
+    
     # function to load the sparse infill network and run a forward pass of one image
     # make tensor for coords (row,col,batch)
     # print "forward pass"
     starttime = time.time()
-    ncoords = np.size(data_t,0)
+    ncoords = np.size(data_t,0)    
     isdead = np.equal(data_t,np.zeros((ncoords,3)))
     isdeadnum = np.sum(isdead)
     # print isdeadnum
     if (isdeadnum == 0):
-        print "SKIPPED DUE TO NO DEAD CHANNELS"
+        print("SKIPPED DUE TO NO DEAD CHANNELS")
         return data_t
     coord_t = torch.ones( (ncoords,3), dtype=torch.int )
     # tensor for input pixels
@@ -50,15 +43,24 @@ def forwardpassu(data_t):
     input_t[0:ncoords,0]  = torch.from_numpy(data_t[:,2])
 
 
-
     # loading model with hard coded parameters used in training
     # ( (height,width),reps,ninput_features, noutput_features,nplanes, show_sizes=False)
-    CHECKPOINT_FILE = "/mnt/disk1/nutufts/kmason/sparsenet/ubdl/sparse_infill/sparse_infill/training/sparseinfill_uplane_test.tar"
+    if CHECKPOINT_FILE is None:
+        CHECKPOINT_FILE = "/mnt/disk1/nutufts/kmason/sparsenet/ubdl/sparse_infill/sparse_infill/training/sparseinfill_uplane_test.tar"
     model = SparseInfill( (512,496), 1,16,16,5, show_sizes=False)
 
     # load checkpoint data
     checkpoint = torch.load( CHECKPOINT_FILE, {"cuda:0":"cpu","cuda:1":"cpu"} )
-    best_prec1 = checkpoint["best_prec1"]
+
+    for name,arr in checkpoint["state_dict"].items():
+        if ( ("unet" in name and "weight" in name and len(arr.shape)==3) or
+             ("conv2" in name and "weight" in name and len(arr.shape)==3) or
+             ("conv1" in name and "weight" in name and len(arr.shape)==3) or
+             ("sparseModel" in name and "weight" in name and len(arr.shape)==3) ):
+            #print("reshaping ",name)
+            checkpoint["state_dict"][name] = arr.reshape( (arr.shape[0], 1, arr.shape[1], arr.shape[2]) )
+
+    
     model.load_state_dict(checkpoint["state_dict"])
     loadedmodeltime = time.time()
 
@@ -73,13 +75,13 @@ def forwardpassu(data_t):
     predict_t[:,0:2] = data_t[:,0:2]
     resizetime = time.time()
     # print "loadtime: ",loadedmodeltime-starttime
-    print "forwardpass: ", forwardpasstime-loadedmodeltime
+    print("forwardpass: ", forwardpasstime-loadedmodeltime)
     # print "resizetime: ", resizetime-forwardpasstime
 
 
     return predict_t
 
-def forwardpassv(data_t):
+def forwardpassv(data_t, CHECKPOINT_FILE=None):
     # function to load the sparse infill network and run a forward pass of one image
     # make tensor for coords (row,col,batch)
     ncoords = np.size(data_t,0)
@@ -87,7 +89,7 @@ def forwardpassv(data_t):
     isdead = np.equal(data_t,np.zeros((ncoords,3)))
     isdeadnum = np.sum(isdead)
     if (isdeadnum == 0):
-        print "SKIPPED DUE TO NO DEAD CHANNELS"
+        print("SKIPPED DUE TO NO DEAD CHANNELS")
         return data_t
     # tensor for input pixels
     input_t = torch.zeros( (ncoords,1), dtype=torch.float)
@@ -98,12 +100,21 @@ def forwardpassv(data_t):
 
     # loading model with hard coded parameters used in training
     # ( (height,width),reps,ninput_features, noutput_features,nplanes, show_sizes=False)
-    CHECKPOINT_FILE = "/mnt/disk1/nutufts/kmason/sparsenet/ubdl/sparse_infill/sparse_infill/training/sparseinfill_vplane_test.tar"
+    if CHECKPOINT_FILE is None:    
+        CHECKPOINT_FILE = "/mnt/disk1/nutufts/kmason/sparsenet/ubdl/sparse_infill/sparse_infill/training/sparseinfill_vplane_test.tar"
     model = SparseInfill( (512,496), 1,16,16,5, show_sizes=False)
 
     # load checkpoint data
     checkpoint = torch.load( CHECKPOINT_FILE, {"cuda:0":"cpu","cuda:1":"cpu"} )
-    best_prec1 = checkpoint["best_prec1"]
+
+    for name,arr in checkpoint["state_dict"].items():
+        if ( ("unet" in name and "weight" in name and len(arr.shape)==3) or
+             ("conv2" in name and "weight" in name and len(arr.shape)==3) or
+             ("conv1" in name and "weight" in name and len(arr.shape)==3) or
+             ("sparseModel" in name and "weight" in name and len(arr.shape)==3) ):
+            #print("reshaping ",name)
+            checkpoint["state_dict"][name] = arr.reshape( (arr.shape[0], 1, arr.shape[1], arr.shape[2]) )
+    
     model.load_state_dict(checkpoint["state_dict"])
     loadedmodeltime = time.time()
 
@@ -115,11 +126,11 @@ def forwardpassv(data_t):
     predict_t = np.zeros( (ncoords,3), dtype=np.float32)
     predict_t[:,2]   = out_t[:,0]
     predict_t[:,0:2] = data_t[:,0:2]
-    print "forwardpass: ", forwardpasstime-loadedmodeltime
+    print("forwardpass: ", forwardpasstime-loadedmodeltime)
 
     return predict_t
 
-def forwardpassy(data_t):
+def forwardpassy(data_t, CHECKPOINT_FILE=None):
     # function to load the sparse infill network and run a forward pass of one image
     # make tensor for coords (row,col,batch)
     ncoords = np.size(data_t,0)
@@ -127,7 +138,7 @@ def forwardpassy(data_t):
     isdead = np.equal(data_t,np.zeros((ncoords,3)))
     isdeadnum = np.sum(isdead)
     if (isdeadnum == 0):
-        print "SKIPPED DUE TO NO DEAD CHANNELS"
+        print("SKIPPED DUE TO NO DEAD CHANNELS")
         return data_t
     # tensor for input pixels
     input_t = torch.zeros( (ncoords,1), dtype=torch.float)
@@ -140,12 +151,21 @@ def forwardpassy(data_t):
 
     # loading model with hard coded parameters used in training
     # ( (height,width),reps,ninput_features, noutput_features,nplanes, show_sizes=False)
-    CHECKPOINT_FILE = "/mnt/disk1/nutufts/kmason/sparsenet/ubdl/sparse_infill/sparse_infill/training/sparseinfill_yplane_test.tar"
+    if CHECKPOINT_FILE is None:
+        CHECKPOINT_FILE = "/mnt/disk1/nutufts/kmason/sparsenet/ubdl/sparse_infill/sparse_infill/training/sparseinfill_yplane_test.tar"
     model = SparseInfill( (512,496), 1,16,16,5, show_sizes=False)
 
     # load checkpoint data
     checkpoint = torch.load( CHECKPOINT_FILE, {"cuda:0":"cpu","cuda:1":"cpu"} )
-    best_prec1 = checkpoint["best_prec1"]
+
+    for name,arr in checkpoint["state_dict"].items():
+        if ( ("unet" in name and "weight" in name and len(arr.shape)==3) or
+             ("conv2" in name and "weight" in name and len(arr.shape)==3) or
+             ("conv1" in name and "weight" in name and len(arr.shape)==3) or
+             ("sparseModel" in name and "weight" in name and len(arr.shape)==3) ):
+            #print("reshaping ",name)
+            checkpoint["state_dict"][name] = arr.reshape( (arr.shape[0], 1, arr.shape[1], arr.shape[2]) )
+    
     model.load_state_dict(checkpoint["state_dict"])
     loadedmodeltime = time.time()
 
@@ -157,6 +177,12 @@ def forwardpassy(data_t):
     predict_t = np.zeros( (ncoords,3), dtype=np.float32)
     predict_t[:,2]   = out_t[:,0]
     predict_t[:,0:2] = data_t[:,0:2]
-    print "forwardpass: ", forwardpasstime-loadedmodeltime
+    print("forwardpass: ", forwardpasstime-loadedmodeltime)
 
     return predict_t
+
+
+if __name__ == "__main__":
+
+    test = np.zeros( (100, 3 ) )
+    forwardpassu( test, "../weights/prelim_june2019/sparseinfill_uplane_test.tar" )
